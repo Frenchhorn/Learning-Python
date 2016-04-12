3c6930552bc8c7c80d6805469660b5fcf012eb6d
Frenchhorn <690698632@qq.com>
2016-04-12 11:00:24 +0800

    添加两个比较有用的脚本，改一下文件夹的名称

---
+++ 小脚本/mydir.py
@@ -0,0 +1,32 @@
+'''
+mydir.py: a module that lists the namespaces of other modules
+'''
+
+seplen = 60
+sepchr = '-'
+
+def listing(module, verbose=True):
+    sepline = sepchr * seplen
+    if verbose and '__file__' in dir(module):
+        print(sepline)
+        print('name:', module.__name__, 'file:', module.__file__)
+        print(sepline)
+
+    count = 0
+    for attr in module.__dict__:
+        print('%02d)  %s' % (count, attr), end = '  ')
+        if attr.startswith('__'):
+            print('\t<built-in name>')
+        else:
+            print('\t', getattr(module, attr))
+        count += 1
+
+    if verbose:
+        print(sepline)
+        print(module.__name__, 'has %d names' % count)
+        print(sepline)
+
+if __name__ == '__main__':
+    import mydir
+    listing(mydir)
+            
