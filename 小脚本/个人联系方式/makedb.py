5f47c7a1d6ae52d7c14645704a2538e8b98e22fc
Frenchhorn <690698632@qq.com>
2016-04-19 21:55:37 +0800

    一个具体的关于类的使用实例，还包括数据的保存

---
+++ 小脚本/个人联系方式/makedb.py
@@ -0,0 +1,11 @@
+#File makedb.py: store Person objects on a shelve database
+from person import Person, Manager
+bob = Person('Bob Smith')
+sue = Person('Sue Jones', job='dev', pay=100000)
+tom = Manager('Tom Jones', 50000)
+
+import shelve
+db = shelve.open('persondb')
+for object in (bob, sue, tom):
+    db[object.name] = object
+db.close()
